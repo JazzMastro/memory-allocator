44b027d48a255592984cc2dd9033b0edd8bd26f5
JazzMastro <jmastroppolito@gmail.com>
2026-08-19 13:03:31 -0400

    free updated

--- myfree.c
+++ myfree.c
@@ -4,13 +4,26 @@ void myfree(void * address) {
     Block * used = (Block *)((char*)address - sizeof(Block)); //we create a bock for the memory that is being freed
 
     used->free = true;
-    Block * temp = first;
 
-    while (temp->next != NULL) {
-        if (address < temp) {
-            temp = temp->next;
-        }
+    if (used < first) {
+        used->next = first;
+        used->prev = NULL;
+        first->prev = used;
+        first = used;
+        return;
     }
 
+    Block * temp = first;
+    while (temp->next != NULL && temp < used) {
+        temp = temp->next;
+    }
+
+    used->next = temp->next;
+    used->prev = temp;
+    
+    if (temp->next != NULL) {
+        temp->next->prev = used;
+    }
+    temp->next = used;
 
 }
